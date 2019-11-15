2572b93e7676176356aebd34fbc6d07d4008cbac
jyyblue <jyyblue1987@outlook.com>
2019-11-15 03:04:13 +0200

    [add] graph ql server

---
+++ graphql.js
@@ -0,0 +1,12 @@
+// graphql.js
+
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+// Construct a schema, using GraphQL schema language
+const fs = require('fs')
+const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'})
+const resolvers = require('./resolvers')
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.graphqlHandler = server.createHandler();
