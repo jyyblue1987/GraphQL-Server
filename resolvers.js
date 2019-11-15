8aaac260a4b06d1fce7905747d6a0b4cc8a7674b
jyyblue <jyyblue1987@outlook.com>
2019-11-15 03:39:17 +0200

    [add] add mutation

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,7 @@
 const db = require('./db')
 const Query = {
     greeting:() => {
-        return "hello from  TutorialsPoint !!!"
+        return "Hello, How are you?"
     },
     students:() => db.students.list(),
 
@@ -9,7 +9,39 @@ const Query = {
     studentById:(root,args,context,info) => {
         //args will contain parameter passed in query
         return db.students.get(args.id);
+    },
+    sayHello:(root,args,context,info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
+    setFavouriteColor:(root,args) => {
+        return  "Your Fav Color is :"+args.color;
     }
 }
 
-module.exports = {Query}
+const Student = {
+    fullName:(root,args,context,info) => {
+       return root.firstName+":"+root.lastName
+    },
+    college:(root) => {
+       return db.colleges.get(root.collegeId);
+    }
+}
+
+const Mutation = {
+    createStudent:(root,args,context,info) => {
+       return db.students.create({collegeId:args.collegeId,
+       firstName:args.firstName,
+       lastName:args.lastName})
+    },
+   
+    // new resolver function
+    addStudent_returns_object:(root,args,context,info) => {
+       const id = db.students.create({
+          collegeId:args.collegeId,
+          firstName:args.firstName,
+          lastName:args.lastName
+       })
+ 
+       return db.students.get(id)
+    }
+ }
+
+module.exports = {Query, Student,Mutation}
