5e9a23bbaffe2326b487a017d958e7d180f78f94
jyyblue <jyyblue1987@outlook.com>
2022-06-15 23:31:26 +0800

    [add] add jwt to graphql

--- resolvers.js
+++ resolvers.js
@@ -1,66 +1,117 @@
+const jsonwebtoken = require('jsonwebtoken');
+const bcrypt = require('bcrypt')
+
+const JWT_SECRET = '12321312321a';
+
 const db = require('./db')
 const Query = {
-    greeting:() => {
+    greeting: () => {
         return "Hello, How are you?"
     },
-    students:() => db.students.list(),
+    students: () => db.students.list(),
 
     //resolver function for studentbyId
-    studentById:(root,args,context,info) => {
+    studentById: (root, args, context, info) => {
         //args will contain parameter passed in query
         return db.students.get(args.id);
     },
-    sayHello:(root,args,context,info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
-    setFavouriteColor:(root,args) => {
-        return  "Your Fav Color is :"+args.color;
+    sayHello: (root, args, context, info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
+    setFavouriteColor: (root, args) => {
+        return "Your Fav Color is :" + args.color;
+    },
+    // fetch the profile of currently authenticated user
+    async me(_, args, { user }) {
+        // make sure user is logged in
+        if (!user) {
+            throw new Error('You are not authenticated!')
+        }
+
+        // user is authenticated
+        return db.students.get(user.id);
     }
 }
 
 const Student = {
-    fullName:(root,args,context,info) => {
-       return root.firstName+":"+root.lastName
+    fullName: (root, args, context, info) => {
+        return root.firstName + ":" + root.lastName
     },
-    college:(root) => {
-       return db.colleges.get(root.collegeId);
+    college: (root) => {
+        return db.colleges.get(root.collegeId);
     }
 }
 
 const Mutation = {
-    createStudent:(root,args,context,info) => {
-       return db.students.create({collegeId:args.collegeId,
-       firstName:args.firstName,
-       lastName:args.lastName})
+    createStudent: (root, args, context, info) => {
+        return db.students.create({
+            collegeId: args.collegeId,
+            firstName: args.firstName,
+            lastName: args.lastName
+        })
     },
-   
+
     // new resolver function
-    addStudent_returns_object:(root,args,context,info) => {
-       const id = db.students.create({
-          collegeId:args.collegeId,
-          firstName:args.firstName,
-          lastName:args.lastName
-       })
- 
-       return db.students.get(id)
+    addStudent_returns_object: (root, args, context, info) => {
+        const id = db.students.create({
+            collegeId: args.collegeId,
+            firstName: args.firstName,
+            lastName: args.lastName
+        })
+
+        return db.students.get(id)
     },
 
-    signUp:(root,args,context,info) => {
+    signUp: async (root, args, context, info) => {
 
-        const {email,firstName,password} = args.input;
-  
+        const { email, firstName, password } = args.input;
+
         const emailExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        
-        const isValidEmail =  emailExpression.test(String(email).toLowerCase())
-        if(!isValidEmail)
-            throw new Error("email not in proper format")
-  
-        if(firstName.length > 15)
-            throw new Error("firstName should be less than 15 characters")
-  
-        if(password.length < 8 )
-            throw new Error("password should be minimum 8 characters")
-        
-        return "success";
-     }
- }
 
-module.exports = {Query, Student,Mutation}
+        const isValidEmail = emailExpression.test(String(email).toLowerCase())
+        if (!isValidEmail)
+            throw new Error("email not in proper format")
+
+        if (firstName.length > 15)
+            throw new Error("firstName should be less than 15 characters")
+
+        if (password.length < 8)
+            throw new Error("password should be minimum 8 characters")
+
+        const id = db.students.create({
+            email: email,
+            firstName: firstName,
+            password: await bcrypt.hash(password, 10)
+        })
+
+        // // return json web token
+        return jsonwebtoken.sign(
+            { id: id, email: email },
+            JWT_SECRET,
+            { algorithm: 'HS256' }
+        )
+    },
+
+    login: async (root, args, context, info) => {
+
+        const { email, password } = args;
+
+        const user = db.students.list().find(row => row.email == email);
+
+        if (!user) {
+            throw new Error('No user with that email')
+        }
+
+        const valid = await bcrypt.compare(password, user.password);
+
+        if (!valid) {
+            throw new Error('Incorrect password')
+        }
+
+        return jsonwebtoken.sign(
+            { id: user.id, email: user.email },
+            JWT_SECRET,
+            { algorithm: 'HS256' }
+        )
+    },
+}
+
+module.exports = { Query, Student, Mutation }
--- server.js
+++ server.js
@@ -1,27 +1,60 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const db = require('./db');
+const jwt = require("jsonwebtoken");
+
+// const db = require('./db');
+const JWT_SECRET = '12321312321a';
 
 const port = process.env.PORT || 9000;
 const app = express();
 
+// // auth middleware
+// const auth = jwt({
+//    secret: JWT_SECRET,
+//    algorithms: ["HS256"],
+//    credentialsRequired: false
+// })
+
+
 
 // Construct a schema, using GraphQL schema language
 const fs = require('fs')
-const typeDefs = fs.readFileSync('./schema.graphql',{encoding:'utf-8'})
+const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf-8' })
 const resolvers = require('./resolvers')
 // ====================================================
 
 
-const {makeExecutableSchema} = require('graphql-tools')
-const schema = makeExecutableSchema({typeDefs, resolvers})
+const { makeExecutableSchema } = require('graphql-tools')
+const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 app.use(cors(), bodyParser.json());
 
-const  {graphiqlExpress,graphqlExpress} = require('apollo-server-express')
-app.use('/graphql',graphqlExpress({schema}))
-app.use('/graphiql',graphiqlExpress({endpointURL:'/graphql'}))
+const verifyToken = (req, res, next) => {
+   // console.log(req)
+   let token = req.headers["x-access-token"];
+   if (!token) {
+      next()
+      return;
+   }
+
+   jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (!err) {
+         req.user = decoded
+      }
+
+      next();
+   });
+};
+
+const { graphiqlExpress, graphqlExpress } = require('apollo-server-express')
+app.use('/graphql', bodyParser.json(), verifyToken, graphqlExpress(req => ({
+   schema,
+   context: {
+      user: req.user
+   }
+})))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 app.listen(
    port, () => console.info(
